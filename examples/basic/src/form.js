a2ecc9fb23562d1473d9fc768ba6d93fbb21bc7c
maebe22 <maeby1115@gmail.com>
2015-04-21 05:49:55 +0800

    added list for fields with errors

--- examples/basic/src/form.js
+++ examples/basic/src/form.js
@@ -16,22 +16,33 @@ var Form = React.createClass({
 
     var errorMessage = {
       width: 10,
-      height: 10,
+      height: 6,
       border: '1px solid red',
-      marginTop: 10
+      marginTop: 10, 
+      marginLeft: 10,
+      display: 'inline'
     }
+    console.log(errors);
     return (
-      <form onSubmit={this.handleSubmit}>
-        {errors.username ?  <span style={errorMessage}>username is {errors.username} </span> : ''} 
+      <form onSubmit={this.handleSubmit}> 
+        { errors ? 
+          <div>
+            <span>Listed fields with errors</span>
+            <ul>{ Object.keys(errors).map(function(key, i) {
+              return <li key={i}>{ key }</li>
+            }) }
+            </ul>
+          </div> : '' }      
         <div style={{ marginBottom: 10}}>
           <label> Username: </label> 
           <input type="text" valueLink={this.linkState('username')} style={{ border: errors.username ? '1px solid red' : '', width: 150 }} />
+          {errors.username ?  <span style={errorMessage}>{ errors.username === 'required' ? 'Username is required' : 'Username should be ' + errors.username }</span> : ''}
         </div>
 
-        {errors.passowrd ?  <span style={errorMessage}>password is {errors.password} </span> : ''} 
         <div style={{ marginBottom: 10}}>
           <label> Password: </label>
           <input type="password" valueLink={this.linkState('password')} style={{ border: errors.password ? '1px solid red' : '', width: 150 }} />
+          {errors.password ?  <span style={errorMessage}>{ errors.password === 'required' ? 'Password is required' : 'Password should be ' + errors.password } </span> : ''} 
         </div>
 
         <div style={{ marginBottom: 10}}>
@@ -50,12 +61,12 @@ var Form = React.createClass({
     };
 
     var rules = {
-      username: iz(data.username, 'sir required').required(),
-      password: iz(data.password, 'sir required').required()
+      username: iz(data.username).required().alphaNumeric(),
+      password: iz(data.password).required().alphaNumeric()
     };
 
     var areRules = are(rules);
-    this.setState({ errors: areRules.getInvalidFields() })
+    this.setState({ errors: areRules.getInvalidFields() });
   }
 });
 
