d7c183ff6bf4d1370ff6c2060d23a27c817c7002
maebe22 <maeby1115@gmail.com>
2015-04-23 05:55:56 +0800

    fixed some bugs and fixed example view

--- examples/basic/src/form.js
+++ examples/basic/src/form.js
@@ -1,43 +1,49 @@
-var React = require('react');
-var ReactIz = require('../../src/ReactIz');
+var React = require('react/addons');
+var LinkedStateMixin = React.addons.LinkedStateMixin;
+var ReactIz = require('../../../src/ReactIz');
 
 var rules = {
   'username': [
-    { 'rule': 'required' },
-    { 'rule': 'alphaNumeric' }
+    { 'rule': 'required', 'error': 'Username is required' },
+    { 'rule': 'alphaNumeric', 'error': 'Username should consist of numbers and letters' }
   ],
   'password': [
-    { 'rule': 'required' },
-    { 'rule': 'alphaNumeric' }
+    { 'rule': 'required', 'error': 'Password is required' },
+    { 'rule': 'alphaNumeric', 'error': 'Password should consist of numbers and letters' }
   ]
 };
 
 var Example = React.createClass({
+  mixins: [LinkedStateMixin],
+
   getInitialState: function() {
     return {};
-  }
+  },
+
   render: function() {
-    <ReactIz data={this.state} rules={rules} render{(props) => {
+    return (<ReactIz data={this.state} rules={rules} render={ (props) => {
       return (
-        <form onChange={this.handleValidation}>
-          <label> Username </label>
-          { this.hasErrors() ? <div>Please kindly fix the errors</div> : '' }
-          <input type="text" valueLink={this.linkState('username')} />
-          { this.hasError('username') ? <div> { this.getError('username') }</div> : '' }
+        <form onSubmit={(evt) => { evt.preventDefault(); props.handleValidation() }}>
+          { props.hasErrors() ? <div>Please kindly fix the errors</div> : '' }
+
+          <label> Username: </label>
+          <input type="text" valueLink={this.linkState('username')} style={{display: 'block'}}/>
+          { props.hasError('username') ? <div style={{border: '1px solid red', width: 158}}> { props.getError('username') }</div> : '' }
           
-          <label> Password </label>
-          <input type="password" valueLink={this.linkState('password')} />
-          { this.hasError('password') ? <div> { this.getError('password') }</div> : '' }
+          <label> Password: </label>
+          <input type="password" valueLink={this.linkState('password')} style={{display: 'block'}}/>
+          { props.hasError('password') ? <div style={{border: '1px solid red', width: 158}}> { props.getError('password') }</div> : '' }
+
+          <button type="submit">Submit</button>
         </form>
       );
     }} />
+  );
   }
-
-
 });
 
 
 React.render(
   <Example />,
-  document,body
+  document.body
 );
